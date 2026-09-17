3cc2b010f29bc9bc3fc747bc0a9bb6d66da4cc72
Joshua Pook <joshua.pook@madetech.com>
2026-09-17 17:17:08 +0100

    WIP - Updated PayoutUpdated to add estimatedArrivalDate

---
+++ src/main/java/uk/gov/pay/connector/events/eventdetails/payout/PayoutUpdatedEventDetails.java
@@ -0,0 +1,16 @@
+package uk.gov.pay.connector.events.eventdetails.payout;
+
+import uk.gov.pay.connector.events.eventdetails.EventDetails;
+
+public class PayoutUpdatedEventDetails extends EventDetails {
+
+    private String estimatedArrivalDate;
+
+    public PayoutUpdatedEventDetails(String gatewayStatus) {
+        this.estimatedArrivalDate = gatewayStatus;
+    }
+
+    public String getGatewayStatus() {
+        return gatewayStatus;
+    }
+}
