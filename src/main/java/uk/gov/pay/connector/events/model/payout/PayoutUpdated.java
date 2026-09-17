0355a92dfe8ad8135310543ab314c6e2c6e7f00c
Joshua Pook <joshua.pook@madetech.com>
2026-09-17 17:17:08 +0100

    WIP - Updated PayoutUpdated to add estimatedArrivalDate

--- src/main/java/uk/gov/pay/connector/events/model/payout/PayoutUpdated.java
+++ src/main/java/uk/gov/pay/connector/events/model/payout/PayoutUpdated.java
@@ -1,7 +1,7 @@
 package uk.gov.pay.connector.events.model.payout;
 
 import uk.gov.pay.connector.events.eventdetails.payout.PayoutEventWithGatewayStatusDetails;
-import uk.gov.pay.connector.events.eventdetails.payout.PayoutUpdatedEventDetails;
+import uk.gov.pay.connector.events.eventdetails.payout.PayoutEventWithArrivalDateDetails;
 import uk.gov.pay.connector.gateway.adyen.response.transfer.AdyenTransferData;
 import uk.gov.pay.connector.gateway.stripe.json.StripePayout;
 
@@ -12,19 +12,25 @@ public class PayoutUpdated extends PayoutEvent {
         super(resourceExternalId, eventDetails, timestamp);
     }
 
+    public PayoutUpdated(String resourceExternalId, PayoutEventWithArrivalDateDetails eventDetails, Instant timestamp) {
+        super(resourceExternalId, eventDetails, timestamp);
+    }
+
     public static PayoutUpdated from(Instant eventTimestamp, StripePayout payout) {
         return new PayoutUpdated(payout.getId(),
                 new PayoutEventWithGatewayStatusDetails(payout.getStatus()),
                 eventTimestamp);
     }
 
-    public static PayoutUpdated from(Instant eventTimestamp, AdyenTransferData transferEventData) {
-        return new PayoutUpdated(transferEventData.id(),
-                new PayoutUpdatedEventDetails(transferEventData.tracking().estimatedArrivalTime()),
-                eventTimestamp);
+    public static PayoutUpdated fromUsingArrivalTime(Instant eventTimestamp, AdyenTransferData transferEventData) {
+            Instant estimatedArrivalTime = Instant.parse(transferEventData.tracking().estimatedArrivalTime());
+            
+            return new PayoutUpdated(transferEventData.id(),
+                    new PayoutEventWithArrivalDateDetails(estimatedArrivalTime),
+                    eventTimestamp);
     }
 
-    public static PayoutUpdated from(Instant eventTimestamp, AdyenTransferData transferEventData) {
+    public static PayoutUpdated fromUsingStatus(Instant eventTimestamp, AdyenTransferData transferEventData) {
         return new PayoutUpdated(transferEventData.id(),
                 new PayoutEventWithGatewayStatusDetails(transferEventData.status()),
                 eventTimestamp);
