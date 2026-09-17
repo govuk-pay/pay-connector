1ba436e211b3f9aa5367ecfdbff6a6c3b8bb0891
Joshua Pook <joshua.pook@madetech.com>
2026-09-17 17:16:22 +0100

    WIP - Updated PayoutUpdated to add estimatedArrivalDate

--- src/main/java/uk/gov/pay/connector/events/model/payout/PayoutUpdated.java
+++ src/main/java/uk/gov/pay/connector/events/model/payout/PayoutUpdated.java
@@ -1,6 +1,8 @@
 package uk.gov.pay.connector.events.model.payout;
 
 import uk.gov.pay.connector.events.eventdetails.payout.PayoutEventWithGatewayStatusDetails;
+import uk.gov.pay.connector.events.eventdetails.payout.PayoutUpdatedEventDetails;
+import uk.gov.pay.connector.gateway.adyen.response.transfer.AdyenTransferData;
 import uk.gov.pay.connector.gateway.stripe.json.StripePayout;
 
 import java.time.Instant;
@@ -13,6 +15,18 @@ public class PayoutUpdated extends PayoutEvent {
     public static PayoutUpdated from(Instant eventTimestamp, StripePayout payout) {
         return new PayoutUpdated(payout.getId(),
                 new PayoutEventWithGatewayStatusDetails(payout.getStatus()),
+                eventTimestamp);
+    }
+
+    public static PayoutUpdated from(Instant eventTimestamp, AdyenTransferData transferEventData) {
+        return new PayoutUpdated(transferEventData.id(),
+                new PayoutUpdatedEventDetails(transferEventData.tracking().estimatedArrivalTime()),
+                eventTimestamp);
+    }
+
+    public static PayoutUpdated from(Instant eventTimestamp, AdyenTransferData transferEventData) {
+        return new PayoutUpdated(transferEventData.id(),
+                new PayoutEventWithGatewayStatusDetails(transferEventData.status()),
                 eventTimestamp);
     }
 }
